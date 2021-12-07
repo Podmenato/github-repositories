19eeffc016ba5b0dc215c856ef4a3c7aa8f86ffe
podmenato <filipfedin@gmail.com>
2021-12-07 21:46:14 +0100

    Adding BrowserRouter to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
 
 import theme from 'theme';
+import Routing from 'components/Routing';
 
 const App = () => (
 	<ThemeProvider theme={theme}>
 		<CssBaseline />
+		<BrowserRouter>
+			<Routing />
+		</BrowserRouter>
 	</ThemeProvider>
 );
 
