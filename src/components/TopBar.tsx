1673a137cd6b617a465db68799d6497b9d72c758
podmenato <filipfedin@gmail.com>
2021-12-07 22:58:06 +0100

    Removing Login button from TopBar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -7,7 +7,6 @@ const TopBar: FC = () => (
 			<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
 				GH Repos
 			</Typography>
-			<Button color="inherit">Login</Button>
 		</Toolbar>
 	</AppBar>
 );
