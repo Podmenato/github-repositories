aaa59f3645c067ffa3378e6dab4981afad4230f4
podmenato <filipfedin@gmail.com>
2021-12-08 18:15:40 +0100

    Adding navigation buttons to topbar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,12 +1,38 @@
 import React, { FC } from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Button, Container, Toolbar, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+import { EPaths } from 'enums/EPaths';
 
 const TopBar: FC = () => (
 	<AppBar position="static">
-		<Toolbar>
-			<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-				GH Repos
+		<Toolbar disableGutters sx={{ gap: 2 }}>
+			<Typography
+				variant="h6"
+				component="div"
+				color="secondary"
+				sx={{ flexGrow: 1, marginLeft: '20px' }}
+			>
+				GitHub Repos
 			</Typography>
+			<Container>
+				<Button
+					color="secondary"
+					variant="text"
+					component={Link}
+					to={EPaths.HOME}
+				>
+					Home
+				</Button>
+				<Button
+					color="secondary"
+					variant="text"
+					component={Link}
+					to={EPaths.USERS}
+				>
+					Users
+				</Button>
+			</Container>
 		</Toolbar>
 	</AppBar>
 );
