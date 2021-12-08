588a677d6a1af476b002e9c0b6a005e71273bc13
podmenato <filipfedin@gmail.com>
2021-12-08 19:04:19 +0100

    Adding footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,24 @@
 import React, { FC } from 'react';
+import { Box, Container } from '@mui/material';
 
-const Footer: FC = () => <div />;
+const Footer: FC = () => (
+	<Box
+		px={{ xs: 1, sm: 3 }}
+		py={{ xs: 1, sm: 3 }}
+		bgcolor="footer"
+		color="secondary.main"
+		minHeight="10vh"
+		alignContent="center"
+		justifyContent="center"
+		alignItems="center"
+		justifyItems="center"
+	>
+		<Container maxWidth="lg">
+			<Box textAlign="center">
+				GitHub repos {new Date().getFullYear()} by Filip Daniel Fedin
+			</Box>
+		</Container>
+	</Box>
+);
 
 export default Footer;
--- src/theme.ts
+++ src/theme.ts
@@ -4,10 +4,12 @@ declare module '@mui/material/styles' {
 	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 	interface Palette {
 		bgPrimary?: string;
+		footer?: string;
 	}
 	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 	interface PaletteOptions {
 		bgPrimary?: string;
+		footer?: string;
 	}
 }
 
@@ -15,7 +17,8 @@ const theme = createTheme({
 	palette: {
 		primary: { main: '#6a1b9a' },
 		secondary: { main: '#ffeb3b' },
-		bgPrimary: '#deb0fd'
+		bgPrimary: '#deb0fd',
+		footer: '#5c0097'
 	}
 });
 
