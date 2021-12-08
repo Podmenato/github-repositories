22ef62620da1172c2579b1b65624c4ac17c6f7ce
podmenato <filipfedin@gmail.com>
2021-12-08 18:20:07 +0100

    Adding text to main page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import { Box, Button, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-const Home = () => <div>HOMEPAGE</div>;
+import { EPaths } from 'enums/EPaths';
+
+const Home = () => (
+	<Box
+		sx={{
+			display: 'flex',
+			flexDirection: 'column',
+			justifyContent: 'center',
+			alignItems: 'center'
+		}}
+	>
+		<Typography variant="h4">
+			This is a simple web application which will fetch data, names of
+			repositories and organization of an GitHub user.
+		</Typography>
+
+		<Button variant="contained" component={Link} to={EPaths.USERS}>
+			Get started
+		</Button>
+	</Box>
+);
 
 export default Home;
