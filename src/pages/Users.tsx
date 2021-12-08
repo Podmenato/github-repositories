6464cfaaa51c064c69c5377b15f59139b89cf9c2
podmenato <filipfedin@gmail.com>
2021-12-08 09:33:05 +0100

    Adding user data fetching in Users component

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Users = () => <div />;
+import { getuserData } from 'api/github-api';
+import { TUser } from 'types/TUser';
+
+const Users = () => {
+	const [userData, setUserData] = useState<TUser | null>(null);
+	useEffect(() => {
+		const fetchData = async () => {
+			const data: TUser = await getuserData('Podmenato');
+			setUserData(data);
+		};
+		fetchData();
+	}, []);
+	return <div />;
+};
 
 export default Users;
